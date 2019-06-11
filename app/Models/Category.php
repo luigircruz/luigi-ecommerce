3624a0aaea82daed623538713575873a33af4193
Alain Luigi R. Cruz <alainluigicruz@gmail.com>
2019-06-12 00:29:17 +0800

    Added columns on migration and defined  and  properties on Category files

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name', 'slug', 'description', 'parent_id', 'featured', 'menu', 'image'
+    ];
+
+    protected $casts = [
+        'parent_id' =>  'integer',
+        'featured'  =>  'boolean',
+        'menu'      =>  'boolean'
+    ];
 }
