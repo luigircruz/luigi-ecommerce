139be0a21b3ca2d8b24e0e374e784380fa5c1418
Luigi Cruz <alainluigicruz@gmail.com>
2019-06-20 13:08:58 +0800

    Created CRUD store on CategoryController

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -43,4 +43,27 @@ class CategoryController extends BaseController
         $this->setPageTitle('Categories', 'Create Category');
         return view('admin.categories.create', compact('categories'));
     }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'parent_id' =>  'required|not_in:0',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $category = $this->categoryRepository->createCategory($params);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Error occurred while creating category.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.categories.index', 'Category added successfully' ,'success',false, false);
+    }
 }
