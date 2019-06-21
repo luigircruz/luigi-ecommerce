40d10ae17c3439fa5befb41e4a0fe6fbb07bcc35
Alain Luigi R. Cruz <alainluigicruz@gmail.com>
2019-06-22 04:36:34 +0800

    Added CRUD Update on CategoryController

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -79,4 +79,27 @@ class CategoryController extends BaseController
         $this->setPageTitle('Categories', 'Edit Category : '.$targetCategory->name);
         return view('admin.categories.edit', compact('categories', 'targetCategory'));
     }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'parent_id' =>  'required|not_in:0',
+            'image'     =>  'mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $category = $this->categoryRepository->updateCategory($params);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Error occurred while updating category.', 'error', true, true);
+        }
+        return $this->responseRedirectBack('Category updated successfully' ,'success',false, false);
+    }
 }
