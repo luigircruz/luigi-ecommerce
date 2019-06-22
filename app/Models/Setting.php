9dbd0935998a5a96e5aee6f0d40a74915aa11c2c
Alain Luigi R. Cruz <alainluigicruz@gmail.com>
2019-06-23 01:30:14 +0800

    Fixed Settings not storing and updating to database

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Config;
 
 class Setting extends Model
 {
+
     /**
      * @var string
      */
--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use App\Models\Setting;
+use Config;
 
 class SettingServiceProvider extends ServiceProvider
 {
