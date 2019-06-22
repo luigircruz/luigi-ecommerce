759abe52b24c7c68077399f3bb28cc43b667e896
Alain Luigi R. Cruz <alainluigicruz@gmail.com>
2019-06-23 00:52:37 +0800

    Created Model for product attribute

---
+++ app/Models/Attribute.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Attribute
+ * @package App\Models
+ */
+class Attribute extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'attributes';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'code', 'name', 'frontend_type', 'is_filterable', 'is_required'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts  = [
+        'is_filterable' =>  'boolean',
+        'is_required'   =>  'boolean',
+    ];
+}
